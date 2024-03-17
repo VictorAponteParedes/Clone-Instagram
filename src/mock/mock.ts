744daab9d7388477d7ea480551088241a774adec
VictorAponteParedes <vaponte520@gmail.com>
2024-03-17 17:50:24 -0300

    Agrega: datos temporales

---
+++ src/mock/mock.ts
@@ -0,0 +1,9 @@
+export const imageFeed = require("../img/astronauta.jpg");
+export const ImagenPerfil = require("../img/perfil_prin.jpeg");
+export const ImagenPerfil2 = require("../img/perfil_2.jpg");
+export const ImagenPerfil3 = require("../img/perfil_3.jpg");
+export const ImagenPerfil4 = require("../img/perfil_4.jpg");
+
+export const ItemPerfil2 = require("../img/autoPerfil.jpg");
+export const ItemPerfil3 = require("../img/casaPerfil.jpg");
+export const ItemPerfil4 = require("../img/citaPerfil.jpg");
