e3437a733ccdfad111b73cd2a966a0d0194679c3
VictorAponteParedes <vaponte520@gmail.com>
2024-03-18 16:24:58 -0300

    Agrega: se llama al componente de botones de navegacion

--- src/screens/Home/home.tsx
+++ src/screens/Home/home.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useRef } from "react";
 
-import { View, Text, Image, ScrollView, StatusBar } from "react-native";
+import { View, Text, Image, ScrollView, Animated } from "react-native";
 import { MenuHeader } from "../../components/Header/header";
 import { EstadoPerfil } from "../../components/Estado-perfil/estado-perfil";
 import { Feed } from "../../components/feed/feed";
+import { CustomBottomMenu } from "../../components/MenuNavegacion/menuNavegacion";
 import {
   imageFeed,
   ImagenPerfil,
@@ -16,32 +17,41 @@ import {
 } from "../../mock/mock";
 
 export const Home = () => {
+  const handleMenuSelect = (section) => {
+    console.log(`Se seleccionó la sección: ${section}`);
+  };
+
   return (
-    <ScrollView>
-      <View style={{ padding: 20, top: 30 }}>
-        <MenuHeader />
-        <EstadoPerfil />
-        <Feed
-          imageFeed={ItemPerfil2}
-          imagePerfil={ImagenPerfil}
-          nombre="Victor"
-        />
-        <Feed
-          imageFeed={imageFeed}
-          imagePerfil={ImagenPerfil2}
-          nombre="Carlos"
-        />
-        <Feed
-          imageFeed={ItemPerfil3}
-          imagePerfil={ImagenPerfil3}
-          nombre="Maria"
-        />
-        <Feed
-          imageFeed={ItemPerfil4}
-          imagePerfil={ImagenPerfil4}
-          nombre="Adalberto"
-        />
+    <View style={{ flex: 1 }}>
+      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+        <View style={{ paddingHorizontal: 10, top: 30 }}>
+          <MenuHeader />
+          <EstadoPerfil />
+          <Feed
+            imageFeed={ItemPerfil2}
+            imagePerfil={ImagenPerfil}
+            nombre="Victor"
+          />
+          <Feed
+            imageFeed={imageFeed}
+            imagePerfil={ImagenPerfil2}
+            nombre="Carlos"
+          />
+          <Feed
+            imageFeed={ItemPerfil3}
+            imagePerfil={ImagenPerfil3}
+            nombre="Maria"
+          />
+          <Feed
+            imageFeed={ItemPerfil4}
+            imagePerfil={ImagenPerfil4}
+            nombre="Adalberto"
+          />
+        </View>
+      </ScrollView>
+      <View style={{ position: "absolute", bottom: 0, width: "100%" }}>
+        <CustomBottomMenu onSelect={handleMenuSelect} />
       </View>
-    </ScrollView>
+    </View>
   );
 };
